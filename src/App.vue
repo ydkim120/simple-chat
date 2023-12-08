9267b3165e672bcd758c4e2ef91de9dbe6e58caa
ydkim <yedami93@gmail.com>
2023-12-08 17:56:01 +0900

    feat - 세션 정보 없으면 로그인 페이지로 redirect (세션 세팅은 아직..)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
   <div id="app">
    <router-view />
+
+   <!-- <ConfirmDialog /> -->
   </div>
 </template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import { supabase as sb } from '@/supabase'
+
+const router = useRouter()
+
+onMounted(() => {
+  pageRedirect ()
+  console.log(`컴포넌트가 마운트 됐습니다.`)
+})
+
+const pageRedirect = () => {
+  const session = sb.auth.currentSession
+  console.log('session ===> ', session)
+
+  if (session === null) { 
+    alert('세션이 만료되어 로그인 페이지로 이동합니다.')
+    return router.push({ name: 'login-user' })
+  } else if (session === undefined) return router.push({ name: 'login-user' })
+  else return false
+}
+
+</script>
+
 
 <style src="./style/global.css" />
